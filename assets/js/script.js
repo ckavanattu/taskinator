cb6ea51cec20b56cb49f45229420f37c4f63c7a2
ckavanattu <chackojk@gmail.com>
2022-04-21 20:55:55 -0700

    add js to capture form input when adding task

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,25 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
 
     event.preventDefault(); //prevents default behaviour of browser which refreshes browser on form submission (which is the event here)
-    console.log(event);
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value; // [these square brackets] are used to querySelect an HTML element by it's attribute, in this case the input name
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className="task-item";
-    listItemEl.textContent="this is a new task.";
+
+    // create div to hold task info and add to list item, give it a class name
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className="task-info";
+    //add html content to div
+    taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+        //innerHTML allows us to add html tags vs textContent which adss text strings
+
+    //add the div with HTML content to the li item
+    listItemEl.appendChild(taskInfoEl);
+
+    //append the li item to the ul 
     tasksToDoEl.appendChild(listItemEl);
 
 };
