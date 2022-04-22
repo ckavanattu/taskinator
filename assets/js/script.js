5865d15930ea04b96e86a33878e23dcadbbc1b5a
ckavanattu <chackojk@gmail.com>
2022-04-22 12:14:36 -0700

    updating develop branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,55 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var taskFormHandler = function (event) {
 
-    var listItemEl = document.createElement("li");
-    listItemEl.className="task-item";
-    listItemEl.textContent="this is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+    event.preventDefault(); //prevents default behaviour of browser which refreshes browser on form submission (which is the event here)
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value; // [these square brackets] are used to querySelect an HTML element by it's attribute, in this case the input name
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //check if input values are empty
+
+    if (!taskNameInput || !taskTypeInput) {
+        alert("you need to fill out the task form!");
+        return false; //stops if function and returns a value of false
+    }
+
+    formEl.reset();//clears the form after the task is submitted, reset is a DOM method specific to form elements
+    
+    //package the name and type into an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send the object as an argument to create task
+    createTaskEl(taskDataObj);
+
+
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+var createTaskEl = function(taskDataObj) {
 
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className="task-item"
+
+    // create div to hold task info and add to list item, give it a class name
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className="task-info";
+    //add html content to div
+    taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+        //innerHTML allows us to add html tags vs textContent which adss text strings
+
+    //add the div with HTML content to the li item
+    listItemEl.appendChild(taskInfoEl);
+
+    //append the li item to the ul
+    tasksToDoEl.appendChild(listItemEl);
+
+
+
+}
+
+formEl.addEventListener("submit", taskFormHandler); //submit listener works when user clicks button with type 'submit' or enter is pressed
