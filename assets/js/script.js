9844ac5c475a42e40a78254df547a617899fc460
ckavanattu <chackojk@gmail.com>
2022-04-21 20:25:32 -0700

    added form input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
 
+    event.preventDefault(); //prevents default behaviour of browser which refreshes browser on form submission (which is the event here)
+    console.log(event);
     var listItemEl = document.createElement("li");
     listItemEl.className="task-item";
     listItemEl.textContent="this is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler); //submit listener works when user clicks button with type 'submit' or enter is pressed
 
